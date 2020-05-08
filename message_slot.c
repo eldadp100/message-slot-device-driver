03cfa4585e07a6f42292078873cfc4b3176fc9d7
Eldad Peretz <eldadp100@gmail.com>
2020-05-08 18:59:03 +0300

    try different makefile

--- message_slot.c
+++ message_slot.c
@@ -38,8 +38,8 @@ typedef struct simple_dict
 
 simple_dict_t *create_simple_dict(int max_number_of_keys, int sizeof_value)
 {
-    simple_dict_t *dict = malloc(sizeof(simple_dict_t));
-    dict->entries = malloc(max_number_of_keys * sizeof(int) + sizeof_value);
+    simple_dict_t *dict = vmalloc(sizeof(simple_dict_t));
+    dict->entries = vmalloc(max_number_of_keys * sizeof(int) + sizeof_value);
     dict->current_entry_index = 0;
     dict->max_etries = max_number_of_keys;
     return dict;
@@ -60,7 +60,7 @@ void set_value(simple_dict_t *dict, int key, int value)
     if (dict->current_entry_index == dict->max_etries - 1)
     {
         printk(KERN_ERR "simple dict is out of bounds.\n");
-        exit(1);
+        //exit(1);
     }
     dict->values[key] = value;
     dict->dict_size++;
@@ -74,18 +74,17 @@ typedef struct channels
 
 channels_t create_new_channel()
 {
-    channels_t *new_channels = malloc(sizeof(channels_t));
-    new_channels->message_channels = malloc(CHANNELS_NUM * sizeof(char *));
+    channels_t *new_channels = vmalloc(sizeof(channels_t));
+    new_channels->message_channels = vmalloc(CHANNELS_NUM * sizeof(char *));
     new_channels->current_channel = 0;
     return new_channels;
 }
 
 simple_dict_t *minors_to_channels = NULL;
 
-int initialize_minor_to_channels(int number_of_minors)
+void initialize_minor_to_channels(int number_of_minors)
 {
     minors_to_channels = create_simple_dict();
-    return 0;
 }
 
 channels_t *get_channels_obj_of_minor(int minor_num, channels_t *out)
@@ -93,16 +92,15 @@ channels_t *get_channels_obj_of_minor(int minor_num, channels_t *out)
     if (minors_to_channels == NULL)
     {
         printk(KERN_ERR "minors_to_channels object is not initialized\n");
-        exit(1);
+        //exit(1);
     }
     return (*channels_t)get_value(minors_to_channels, minor_num);
 }
 
-int add_minor(int minor_num)
+void add_minor(int minor_num)
 {
     channels_t *new_channel = create_new_channel();
     set_value(minors_to_channels, minor_num, new_channel);
-    return 0;
 }
 
 static spinlock_t device_lock;
@@ -144,18 +142,21 @@ int device_ioctal(struct file *_file, unsigned int control_command, unsigned lon
         if ((channels = get_channels_obj_of_minor(minors_to_channels)) == NULL)
         {
             printk("minor channels should be initialized\n");
-            exit(1);
+            //exit(1);
         }
         _channels->current_channel = command_parameter;
     }
+    return 0;
 }
 
 int device_read(struct file *_file, char *buff, size_t *buff_size, loff_t *file_offset)
 {
+    return 0;
 }
 
 int device_write(struct file *_file, char *buff, size_t *buff_size, loff_t *file_offset)
 {
+    return 0;
 }
 
 void free_minors_data(minors_data_t *_minors_data)
@@ -166,8 +167,10 @@ int device_release()
 {
     unsigned long flags = 0;
     spin_lock_irqsave(&device_lock, flags);
-    free_minors_data(minors_data_t * _minors_data)-- dev_open_flag; // the driver is free to talk with another process
+    free_minors_data(minors_data_t * _minors_data)--;
+    dev_open_flag; // the driver is free to talk with another process
     spin_unlock_irqrestore(&device_lock, flags);
+    return 0;
 }
 
 // DEVICE SETUP (as we saw in recitation 6 chardev 1)
